fb4ac9e50227c538de294539a15e32d5b3fc6b84
boloorbahar <96650873+boloorbahar@users.noreply.github.com>
2025-02-16 21:45:39 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { initData, WebApp } from "@twa-dev/sdk";
+import  WebApp  from "@twa-dev/sdk";
 
 function App() {
   const [user, setUser] = useState(null);
