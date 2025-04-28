a767b8bf50005d43e8ab6bcce9c1753edeae04b7
bahareh boloorchifard <boloorchifard@iidic.com>
2025-04-28 21:11:12 +0330

    fix

--- src/container/fundDetail/fundDetail.js
+++ src/container/fundDetail/fundDetail.js
@@ -11,61 +11,60 @@ import UserAssetCard from "components/userAssetsCard/userAssetsCard";
 
 const FundDetail = () => {
   const { id } = useParams();
-  // const { instance } = useAppContext();
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
-  // const [loading, setLoading] = useState(false);
-  // const [chartData, setChartData] = useState(false);
-  // const data = useSelector((state) => state?.fundReducer?.data);
-  // const balance = useSelector((state) => state?.balanceReducer?.data);
-  // const balanceData =
-  //   balance?.balances?.length > 0 ?
-  //     balance?.balances?.filter((item) => item.chest === id)[0] : null;
+  const { instance } = useAppContext();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [chartData, setChartData] = useState(false);
+  const data = useSelector((state) => state?.fundReducer?.data);
+  const balance = useSelector((state) => state?.balanceReducer?.data);
+  const balanceData =
+    balance?.balances?.length > 0 ?
+      balance?.balances?.filter((item) => item.chest === id)[0] : null;
 
-  // console.log(id, data, balance, balanceData);
-  // const onGetFundDetail = useCallback(async () => {
-  //   if (!id) {
-  //     navigate("/"); // اگر id نیست، کاربر رو برگردون به صفحه اصلی
-  //     return;
-  //   }
-  //   const response = await getFundDetail(instance, id);
-  //   if (response) {
-  //     dispatch(setFund(response));
-  //   }
-  // }, [instance, id]);
+  console.log(id, data, balance, balanceData);
+  const onGetFundDetail = useCallback(async () => {
+    if (!id) {
+      navigate("/"); // اگر id نیست، کاربر رو برگردون به صفحه اصلی
+      return;
+    }
+    const response = await getFundDetail(instance, id);
+    if (response) {
+      dispatch(setFund(response));
+    }
+  }, [instance, id]);
 
-  // useEffect(() => {
-  //   onGetFundDetail();
-  // }, [onGetFundDetail]);
+  useEffect(() => {
+    onGetFundDetail();
+  }, [onGetFundDetail]);
 
-  // const onGetFundChart = useCallback(async () => {
-  //   setLoading(true);
-  //   const response = await getFundChart(instance, id);
-  //   if (response) {
-  //     setChartData(response);
-  //   }
-  //   setLoading(false);
-  // }, [instance, id]);
+  const onGetFundChart = useCallback(async () => {
+    setLoading(true);
+    const response = await getFundChart(instance, id);
+    if (response) {
+      setChartData(response);
+    }
+    setLoading(false);
+  }, [instance, id]);
 
-  // useEffect(() => {
-  //   onGetFundChart();
-  // }, [onGetFundChart]);
+  useEffect(() => {
+    onGetFundChart();
+  }, [onGetFundChart]);
 
-  // const onOrderClick = () => {
-  //   navigate("/order");
-  // };
+  const onOrderClick = () => {
+    navigate("/order");
+  };
 
-  return <div className="text-center text-white mt-10">در حال بارگذاری...</div>;
 
-  // return (
-  //   <>
-  //     <div className=" mb-8 mx-5 ">
-  //       <UserAssetCard data={data} balance={balanceData} hasReturn={true} />
-  //       <ReturnChart data={data} chartData={chartData} loading={loading} />
-  //       <FundInfo data={data} chartData={chartData} />
-  //     </div>
-  //     <Buttons data={balanceData} onClick={onOrderClick} />
-  //   </>
-  // );
+  return (
+    <>
+      <div className=" mb-8 mx-5 ">
+        <UserAssetCard data={data} balance={balanceData} hasReturn={true} />
+        {/* <ReturnChart data={data} chartData={chartData} loading={loading} /> */}
+        <FundInfo data={data} chartData={chartData} />
+      </div>
+      <Buttons data={balanceData} onClick={onOrderClick} />
+    </>
+  );
 };
 export default FundDetail;
