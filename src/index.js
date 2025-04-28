e58040b0d72e03762ae19363aa1a3904985a4a3c
bahareh boloorchifard <boloorchifard@iidic.com>
2025-04-28 20:45:37 +0330

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppProvider } from "utils/context";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
@@ -20,11 +20,11 @@ const persistor = persistStore(store);
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <HashRouter>
+      <BrowserRouter>
         <AppProvider>
           <App />
         </AppProvider>
-      </HashRouter>
+      </BrowserRouter>
     </PersistGate>
   </Provider>
 );
